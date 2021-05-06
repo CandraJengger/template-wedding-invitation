48a077aaac5dad6c49f9dc47116521e6fced1c12
CandraJengger <cakjengger@gmail.com>
2021-05-06 13:17:46 +0700

    Update FAB

--- src/js/components/atoms/FloatingActionButton/index.js
+++ src/js/components/atoms/FloatingActionButton/index.js
@@ -8,7 +8,7 @@ class FloatingActionButton extends HTMLElement {
   render() {
     this.innerHTML = `
       <button
-        class="fixed right-5 bottom-5 md:bottom-14 w-16 h-16 shadow-xl text-xl rounded-full bg-white text-green-dark z-50 transition-all duration-150"
+        class="fixed right-5 bottom-5 md:bottom-14 w-16 h-16 shadow-xl text-xl rounded-full bg-white text-green-dark z-50 transition-all duration-150 play"
         id="${this.idComp}"
       >
         <i class="fas fa-pause"></i>
