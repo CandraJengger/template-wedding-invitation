b13a6ef831ee3831c594560508d580a93ee3d526
CandraJengger <cakjengger@gmail.com>
2021-05-11 12:01:04 +0700

    Update NoInviteComp

--- src/js/components/moleculs/NoInviteComp/index.js
+++ src/js/components/moleculs/NoInviteComp/index.js
@@ -9,9 +9,14 @@ class NoInviteComp extends HTMLElement {
         class="fixed inset-0 bg-white z-50 flex justify-center items-center p-10"
       >
         <img
-          src="../images/content/eastwood-14.png"
+          src="../images/content/leaf.webp"
           alt="Decoration"
-          class="absolute h-72 lg:h-96 -left-7 -bottom-40 md:left-0 md:-bottom-6"
+          class="absolute -left-7 -bottom-14"
+        />
+        <img
+          src="../images/content/leaf.webp"
+          alt="Decoration"
+          class="hidden sm:block absolute -right-7 -bottom-14"
         />
         <div class="text-center z-30 transform translate-y-0 md:translate-y-10">
           <p class="text-xl md:text-2xl text-green-dark mb-6">
