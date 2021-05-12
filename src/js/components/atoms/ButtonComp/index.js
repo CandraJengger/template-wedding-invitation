372e83b93189cd73f61af639277f5233ea2f3890
CandraJengger <cakjengger@gmail.com>
2021-05-12 13:35:01 +0700

    Update layout

--- src/js/components/atoms/ButtonComp/index.js
+++ src/js/components/atoms/ButtonComp/index.js
@@ -15,7 +15,9 @@ class ButtomComp extends HTMLElement {
         id=${this.idComp}
         class="${
           this.variant === 'primary'
-            ? `inline-block btn py-3 px-8 text-center bg-green-700 hover:bg-green-dark text-white 700 transition-all duration-200 my-7 font-semibold rounded-md cursor-pointer ${this.className}`
+            ? `inline-block btn py-3 px-8 text-center bg-green-700 hover:bg-green-dark text-white 700 transition-all duration-200 ${
+                this.type === 'submit' ? '' : 'my-7'
+              } font-semibold rounded-md cursor-pointer ${this.className}`
             : `inline-block bg-yellow-200 hover:bg-transparent text-black hover:text-yellow-200 py-3 px-8 border border-transparent hover:border-yellow-200 rounded-md font-semibold opacity-100 transition-all duration-200 my-6 cursor-pointer ${this.className}`
         }"
         type="${this.type === 'submit' ? 'submit' : ''}"
